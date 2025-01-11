5d4b85776d52a00e9c97ebaeb67adfd798c19682
norbertkiszka <norbert.kiszka@elektrykplakal.pl>
2025-01-11 09:50:23 +0100

    Export device_is_dependent
    Fixes: ERROR: modpost: device_is_dependent [drivers/usb/host/ehci-platform.ko] undefined!

--- drivers/base/core.c
+++ drivers/base/core.c
@@ -293,6 +293,7 @@ int device_is_dependent(struct device *dev, void *target)
 	}
 	return ret;
 }
+EXPORT_SYMBOL(device_is_dependent);
 
 static void device_link_init_status(struct device_link *link,
 				    struct device *consumer,
