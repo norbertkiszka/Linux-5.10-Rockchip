d0739fdc1a646f00afd933246204a1300104a04f
norbertkiszka <norbert.kiszka@elektrykplakal.pl>
2025-01-12 12:19:29 +0100

    Change DT handling in phy-rockchip-pcie again
    Previously changed in 800bf3b4ea724d404a38c78b0bed44de85b6a085

--- drivers/phy/rockchip/phy-rockchip-pcie.c
+++ drivers/phy/rockchip/phy-rockchip-pcie.c
@@ -369,17 +369,15 @@ static int rockchip_pcie_phy_probe(struct platform_device *pdev)
 	int i;
 	u32 phy_num;
 
-	grf = syscon_node_to_regmap(dev->parent->of_node);
+	grf = syscon_regmap_lookup_by_phandle(dev->of_node, "rockchip,grf");
 	if (IS_ERR(grf)) {
-		dev_info(dev, "Cannot find GRF syscon\n");
-		
-		grf = syscon_regmap_lookup_by_phandle(dev->of_node, "rockchip,grf");
+		dev_info(dev, "Missing rockchip,grf property\n");
+		grf = syscon_node_to_regmap(dev->parent->of_node);
 		if (IS_ERR(grf)) {
-			dev_err(dev, "Missing rockchip,grf property\n");
+			dev_err(dev, "Cannot find GRF syscon\n");
 			return PTR_ERR(grf);
 		}
 	}
-	
 
 	rk_phy = devm_kzalloc(dev, sizeof(*rk_phy), GFP_KERNEL);
 	if (!rk_phy)
